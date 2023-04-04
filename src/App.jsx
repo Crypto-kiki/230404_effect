97540c073c3a3d85a8f8dc990db4b1d83f9c8bc3
Crypto-kiki <ejo2123@naver.com>
2023-04-04 11:56:19 +0900

    useEffect counter

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,27 @@
-import ClassComponent from "./components/ClassComponents";
-import FunctionComponent from "./components/FunctionalComponent";
+import { useState } from "react";
+import Counter from "./components/Counter";
+import { useEffect } from "react";
 
 function App() {
+  const [toggle, setToggle] = useState(false);
+
+  const onClickToggle = () => {
+    setToggle(!toggle);
+  };
+
+  useEffect(() => {}, []);
+
   return (
-    <div className="bg-red-100 min-h-screen">
-      <ClassComponent />
-      <FunctionComponent />
+    <div className="bg-red-100 min-h-screen flex flex-col justify-center items-center">
+      {toggle && <Counter />}
+      {/* <counter/> 부분은 값이 있기 때문에 true라고 함 */}
+      {/* {toggle ? <Counter /> : "카운터"} */}
+      <button
+        onClick={onClickToggle}
+        className="bg-pink-300 px-4 py-2 rounded-2xl text-white font-bold mt-4"
+      >
+        버튼
+      </button>
     </div>
   );
 }
--- src/components/FunctionalComponent.jsx
+++
@@ -1,9 +0,0 @@
-import { useEffect } from "react";
-
-const FunctionComponent = () => {
-  useEffect(() => {}, []);
-
-  return <div className="bg-blue-100 h-96">FunctionComponent</div>;
-};
-
-export default FunctionComponent;
